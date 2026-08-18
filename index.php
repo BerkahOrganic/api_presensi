84549a31b8aa3b2c7e8214f590a16826fba81419
HaikalDev27 <haikalapriliano@gmail.com>
2026-08-18 14:58:32 +0700

    mengubah header API

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 declare(strict_types=1);
 
 require_once __DIR__ . '/helpers/response.php';
